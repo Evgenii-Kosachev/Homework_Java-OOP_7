38494ec071f2b3e46a18f391baf60acc78cceb83
EvgeniiKosachev <evgenii_kosachev@mail.ru>
2022-09-16 14:42:59 +0500

    Added a jet ski class

--- src/Model/Hydrocycle.java
+++ src/Model/Hydrocycle.java
@@ -1,2 +1,17 @@
-package Model;public class Hydrocycle {
+package Model;
+
+public class Hydrocycle extends Vehicles{
+    protected String boatHull;
+    protected String waterCannon;
+    protected int lifeJackets;
+
+    public Hydrocycle(String model, String registrationNumber, String engineCylinders,
+                      int gasTank, String fuel, int loadCapacity, int maxSpeed, int weight,
+                      String boatHull, String waterCannon, int lifeJackets) {
+
+        super(model, registrationNumber, engineCylinders, gasTank, fuel, loadCapacity, maxSpeed, weight);
+        this.boatHull = boatHull;
+        this.waterCannon = waterCannon;
+        this.lifeJackets = lifeJackets;
+    }
 }
