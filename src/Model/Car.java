4e65fa0b166cc5b8111f5715a21279b8e89aab35
EvgeniiKosachev <evgenii_kosachev@mail.ru>
2022-09-16 14:40:10 +0500

    Added a car class

--- src/Model/Car.java
+++ src/Model/Car.java
@@ -1,2 +1,19 @@
-package Model;public class Car {
+package Model;
+
+public class Car extends Bike{
+    protected String body;
+    protected int seatBelts;
+    protected String airConditioner;
+
+    public Car(String model, String registrationNumber, String engineCylinders,
+               int gasTank, String fuel, int loadCapacity, int maxSpeed, int weight,
+               String frame, int transmission, String brakeSystem, String suspension,
+               String body, int seatBelts, String airConditioner) {
+
+        super(model, registrationNumber, engineCylinders, gasTank, fuel, loadCapacity,
+                maxSpeed, weight, frame, transmission, brakeSystem, suspension);
+        this.body = body;
+        this.seatBelts = seatBelts;
+        this.airConditioner = airConditioner;
+    }
 }
