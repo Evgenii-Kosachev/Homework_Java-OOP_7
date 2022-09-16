1ef31dbca97637951c08c0665e2544bc9fa5ab87
EvgeniiKosachev <evgenii_kosachev@mail.ru>
2022-09-16 14:41:27 +0500

    Added helicopter class

--- src/Model/Helicopter.java
+++ src/Model/Helicopter.java
@@ -1,2 +1,19 @@
-package Model;public class Helicopter {
+package Model;
+
+public class Helicopter extends Vehicles{
+    protected String fuselage;
+    protected int finalHeight;
+    protected int diameterMainScrew;
+    protected int tailRotorDiameter;
+
+    public Helicopter(String model, String registrationNumber, String engineCylinders,
+                      int gasTank, String fuel, int loadCapacity, int maxSpeed, int weight,
+                      String fuselage, int finalHeight, int diameterMainScrew, int tailRotorDiameter) {
+
+        super(model, registrationNumber, engineCylinders, gasTank, fuel, loadCapacity, maxSpeed, weight);
+        this.fuselage = fuselage;
+        this.finalHeight = finalHeight;
+        this.diameterMainScrew = diameterMainScrew;
+        this.tailRotorDiameter = tailRotorDiameter;
+    }
 }
