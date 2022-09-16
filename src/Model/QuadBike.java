1d0a4b849f345df9ab304b7cd633232688ecadd1
EvgeniiKosachev <evgenii_kosachev@mail.ru>
2022-09-16 14:44:00 +0500

    Added quad bike class

--- src/Model/QuadBike.java
+++ src/Model/QuadBike.java
@@ -1,2 +1,11 @@
-package Model;public class QuadBike {
+package Model;
+
+public class QuadBike extends Bike{
+    public QuadBike(String model, String registrationNumber, String engineCylinders,
+                    int gasTank, String fuel, int loadCapacity, int maxSpeed, int weight,
+                    String frame, int transmission, String brakeSystem, String suspension) {
+
+        super(model, registrationNumber, engineCylinders, gasTank, fuel, loadCapacity,
+                maxSpeed, weight, frame, transmission, brakeSystem, suspension);
+    }
 }
