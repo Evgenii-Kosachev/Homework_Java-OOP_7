cdb287e9d5cde9811447517d3fa64b081bfefde1
EvgeniiKosachev <evgenii_kosachev@mail.ru>
2022-09-16 14:45:27 +0500

    Added a basic technique class

--- src/Model/Vehicles.java
+++ src/Model/Vehicles.java
@@ -1,2 +1,26 @@
-package Model;public class Vehicles {
+package Model;
+
+public abstract class Vehicles {
+    protected String model;
+    protected String registrationNumber;
+
+    protected String engineCylinders;
+    protected int gasTank;
+    protected String fuel;
+
+    protected int loadCapacity;
+    protected int maxSpeed;
+    protected int weight;
+
+    public Vehicles(String model, String registrationNumber, String engineCylinders,
+                    int gasTank, String fuel, int loadCapacity, int maxSpeed, int weight) {
+        this.model = model;
+        this.registrationNumber = registrationNumber;
+        this.engineCylinders = engineCylinders;
+        this.gasTank = gasTank;
+        this.fuel = fuel;
+        this.loadCapacity = loadCapacity;
+        this.maxSpeed = maxSpeed;
+        this.weight = weight;
+    }
 }
