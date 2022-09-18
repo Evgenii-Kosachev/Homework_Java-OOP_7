8148a46a11c05164823dabd0f999311a1e884c3b
EvgeniiKosachev <evgenii_kosachev@mail.ru>
2022-09-18 20:07:28 +0500

    Overrided methods: toString, getMaxSpeed, getEnvironment

--- src/Model/Car.java
+++ src/Model/Car.java
@@ -1,19 +1,47 @@
 package Model;
 
-public class Car extends Bike{
+public class Car extends Bike {
     protected String body;
     protected int seatBelts;
     protected String airConditioner;
 
-    public Car(String model, String registrationNumber, String engineCylinders,
-               int gasTank, String fuel, int loadCapacity, int maxSpeed, int weight,
-               String frame, int transmission, String brakeSystem, String suspension,
-               String body, int seatBelts, String airConditioner) {
-
-        super(model, registrationNumber, engineCylinders, gasTank, fuel, loadCapacity,
-                maxSpeed, weight, frame, transmission, brakeSystem, suspension);
-        this.body = body;
+    public Car(String model, int maxSpeed, String frame, int transmission, String brakeSystem, int seatBelts, String airConditioner) {
+        super(model, maxSpeed, frame, transmission, brakeSystem);
         this.seatBelts = seatBelts;
         this.airConditioner = airConditioner;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", maxSpeed=" + maxSpeed +
+                ", frame='" + frame + '\'' +
+                ", transmission=" + transmission +
+                ", brakeSystem='" + brakeSystem + '\'' +
+                ", seatBelts=" + seatBelts +
+                ", airConditioner='" + airConditioner + '\'' +
+                '}';
+    }
+
+    @Override
+    public String getEnvironment() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", registrationNumber='" + registrationNumber + '\'' +
+                ", maxSpeed=" + maxSpeed +
+                ", frame='" + frame + '\'' +
+                ", body='" + body + '\'' +
+                ", engineCylinders='" + engineCylinders + '\'' +
+                ", transmission=" + transmission +
+                ", brakeSystem='" + brakeSystem + '\'' +
+                ", suspension='" + suspension + '\'' +
+                ", gasTank=" + gasTank +
+                ", fuel='" + fuel + '\'' +
+                ", seatBelts=" + seatBelts +
+                ", airConditioner='" + airConditioner + '\'' +
+                ", loadCapacity=" + loadCapacity +
+                ", weight=" + weight +
+                '}';
     }
 }
