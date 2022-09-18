7d4606264bb8cf7053d4bf3a740fd3b514eb8914
EvgeniiKosachev <evgenii_kosachev@mail.ru>
2022-09-18 20:08:42 +0500

    Overrided methods: toString, getMaxSpeed, getEnvironment

--- src/Model/Hydrocycle.java
+++ src/Model/Hydrocycle.java
@@ -1,17 +1,47 @@
 package Model;
 
-public class Hydrocycle extends Vehicles{
+public class Hydrocycle extends Vehicles {
     protected String boatHull;
     protected String waterCannon;
     protected int lifeJackets;
 
-    public Hydrocycle(String model, String registrationNumber, String engineCylinders,
-                      int gasTank, String fuel, int loadCapacity, int maxSpeed, int weight,
-                      String boatHull, String waterCannon, int lifeJackets) {
-
-        super(model, registrationNumber, engineCylinders, gasTank, fuel, loadCapacity, maxSpeed, weight);
+    public Hydrocycle(String model, int maxSpeed, String boatHull, String waterCannon, int lifeJackets) {
+        super(model, maxSpeed);
         this.boatHull = boatHull;
         this.waterCannon = waterCannon;
         this.lifeJackets = lifeJackets;
+    }
+
+    @Override
+    public String toString() {
+        return "Hydrocycle{" +
+                "model='" + model + '\'' +
+                ", maxSpeed=" + maxSpeed +
+                ", boatHull='" + boatHull + '\'' +
+                ", waterCannon='" + waterCannon + '\'' +
+                ", lifeJackets=" + lifeJackets +
+                '}';
+    }
+
+    @Override
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    @Override
+    public String getEnvironment() {
+        return "Hydrocycle{" +
+                "model='" + model + '\'' +
+                ", registrationNumber='" + registrationNumber + '\'' +
+                ", maxSpeed=" + maxSpeed +
+                ", engineCylinders='" + engineCylinders + '\'' +
+                ", boatHull='" + boatHull + '\'' +
+                ", waterCannon='" + waterCannon + '\'' +
+                ", lifeJackets=" + lifeJackets +
+                ", gasTank=" + gasTank +
+                ", fuel='" + fuel + '\'' +
+                ", loadCapacity=" + loadCapacity +
+                ", weight=" + weight +
+                '}';
     }
 }
