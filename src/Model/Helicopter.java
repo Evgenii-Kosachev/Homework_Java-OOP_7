3e81df73e4e14d69ef4bfe02882c5a90f20b61d5
EvgeniiKosachev <evgenii_kosachev@mail.ru>
2022-09-18 20:08:15 +0500

    Overrided methods: toString, getMaxSpeed, getEnvironment

--- src/Model/Helicopter.java
+++ src/Model/Helicopter.java
@@ -1,19 +1,47 @@
 package Model;
 
-public class Helicopter extends Vehicles{
+public class Helicopter extends Vehicles {
     protected String fuselage;
     protected int finalHeight;
     protected int diameterMainScrew;
     protected int tailRotorDiameter;
 
-    public Helicopter(String model, String registrationNumber, String engineCylinders,
-                      int gasTank, String fuel, int loadCapacity, int maxSpeed, int weight,
-                      String fuselage, int finalHeight, int diameterMainScrew, int tailRotorDiameter) {
-
-        super(model, registrationNumber, engineCylinders, gasTank, fuel, loadCapacity, maxSpeed, weight);
+    public Helicopter(String model, int maxSpeed, String fuselage, int finalHeight) {
+        super(model, maxSpeed);
         this.fuselage = fuselage;
         this.finalHeight = finalHeight;
-        this.diameterMainScrew = diameterMainScrew;
-        this.tailRotorDiameter = tailRotorDiameter;
+    }
+
+    @Override
+    public String toString() {
+        return "Helicopter{" +
+                "model='" + model + '\'' +
+                ", maxSpeed=" + maxSpeed +
+                ", fuselage='" + fuselage + '\'' +
+                ", finalHeight=" + finalHeight +
+                '}';
+    }
+
+    @Override
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    @Override
+    public String getEnvironment() {
+        return "Helicopter{" +
+                "model='" + model + '\'' +
+                ", registrationNumber='" + registrationNumber + '\'' +
+                ", maxSpeed=" + maxSpeed +
+                ", engineCylinders='" + engineCylinders + '\'' +
+                ", fuselage='" + fuselage + '\'' +
+                ", finalHeight=" + finalHeight +
+                ", diameterMainScrew=" + diameterMainScrew +
+                ", tailRotorDiameter=" + tailRotorDiameter +
+                ", gasTank=" + gasTank +
+                ", fuel='" + fuel + '\'' +
+                ", loadCapacity=" + loadCapacity +
+                ", weight=" + weight +
+                '}';
     }
 }
