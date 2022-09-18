4cba09d91df8724f9f215632872282bf04333740
EvgeniiKosachev <evgenii_kosachev@mail.ru>
2022-09-18 20:06:51 +0500

    Overrided methods: toString, getMaxSpeed, getEnvironment

--- src/Model/Bike.java
+++ src/Model/Bike.java
@@ -1,19 +1,49 @@
 package Model;
 
-public class Bike extends Vehicles{
+public class Bike extends Vehicles {
     protected String frame;
     protected int transmission;
     protected String brakeSystem;
     protected String suspension;
 
-    public Bike(String model, String registrationNumber, String engineCylinders,
-                int gasTank, String fuel, int loadCapacity, int maxSpeed, int weight,
-                String frame, int transmission, String brakeSystem, String suspension) {
-
-        super(model, registrationNumber, engineCylinders, gasTank, fuel, loadCapacity, maxSpeed, weight);
+    public Bike(String model, int maxSpeed, String frame, int transmission, String brakeSystem) {
+        super(model, maxSpeed);
         this.frame = frame;
         this.transmission = transmission;
         this.brakeSystem = brakeSystem;
-        this.suspension = suspension;
+    }
+
+    @Override
+    public String toString() {
+        return "Bike{" +
+                "model='" + model + '\'' +
+                ", maxSpeed=" + maxSpeed +
+                ", frame='" + frame + '\'' +
+                ", transmission=" + transmission +
+                ", brakeSystem='" + brakeSystem + '\'' +
+                '}';
+    }
+
+    @Override
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    @Override
+    public String getEnvironment() {
+        return "Bike{" +
+                "model='" + model + '\'' +
+                ", registrationNumber='" + registrationNumber + '\'' +
+                ", maxSpeed=" + maxSpeed +
+                ", frame='" + frame + '\'' +
+                ", engineCylinders='" + engineCylinders + '\'' +
+                ", transmission=" + transmission +
+                ", brakeSystem='" + brakeSystem + '\'' +
+                ", suspension='" + suspension + '\'' +
+                ", gasTank=" + gasTank +
+                ", fuel='" + fuel + '\'' +
+                ", loadCapacity=" + loadCapacity +
+                ", weight=" + weight +
+                '}';
     }
 }
